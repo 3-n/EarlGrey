99133188a915c0eb9edd201d0a2010d8005379e0
Jaroslaw Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2020-04-08 12:30:33 +0200

    Add EarlGrey framework target

---
+++ EarlGrey/EarlGrey.h
@@ -0,0 +1,18 @@
+//
+//  EarlGrey.h
+//  EarlGrey
+//
+//  Created by Jaroslaw Gliwinski on 2020-04-08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EarlGrey.
+FOUNDATION_EXPORT double EarlGreyVersionNumber;
+
+//! Project version string for EarlGrey.
+FOUNDATION_EXPORT const unsigned char EarlGreyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EarlGrey/PublicHeader.h>
+
+
